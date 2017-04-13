67e3035f42131f67c6cbe6bfa5317d2be680748c
JVilla10 <jvilla10@ucol.mx>
2017-04-13 11:55:52 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlDeInventarios
+{
+    public partial class Form1 : Form
+    {
+        Producto miProducto;
+        Inventario miInventario = new Inventario();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool existe = miInventario.eliminarProducto(Convert.ToInt16(txtEliminar.Text));
+
+            if (txtEliminar.Text == "")
+            {
+                MessageBox.Show("Ingrese el número del elemento a eliminar.");
+            }
+            else if(existe == false)
+            {
+                MessageBox.Show("No existe el producto con el código: " + txtEliminar.Text);
+            }
+            else
+            {
+                miInventario.eliminarProducto(Convert.ToInt16(txtEliminar.Text));
+                MessageBox.Show("Producto eliminado");
+                txtEliminar.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            miInventario.insertarProducto(miProducto, Convert.ToInt16(txtPosicion.Text));
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            miProducto = new Producto(Convert.ToInt16(txtCodigo.Text), txtNombre.Text, Convert.ToInt16(txtCantidad.Text), Convert.ToInt16(txtCosto.Text));
+            bool inventario = miInventario.agregarProducto(miProducto);
+
+            if(inventario == true)
+            {
+                miInventario.agregarProducto(miProducto);
+            }
+            else
+            {
+                MessageBox.Show("Ya existe un producto con el código: " + txtCodigo.Text);
+            }
+
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtCantidad.Clear();
+            txtCosto.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = miInventario.ToString();
+        }
+
+        private void btnBuscarPorCodigo_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = miInventario.buscarProducto(Convert.ToUInt16(txtBuscarPorCodigo.Text));
+        }
+    }
+}
---
+++ Inventario.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDeInventarios
+{
+    class Inventario
+    {
+        Producto producto;
+        private Producto[] _Inventario;
+        private int _contador;
+        Producto auxiliar;
+
+        public Inventario()
+        {
+            _Inventario = new Producto[15];
+            _contador = 0;
+        }
+
+        public bool agregarProducto(Producto producto)
+        {
+            bool agregar = true;
+
+            for (int i = 0; i < _contador; i++)
+            {
+                if (producto.codigo == _Inventario[i].codigo)
+                {
+                    agregar = false;
+                }
+            }
+
+            if (_contador < 15 && agregar == true)
+            {
+                _Inventario[_contador] = producto;
+                _contador++;
+            }
+
+            return agregar;
+        }
+
+        public bool eliminarProducto(int posicion)
+        {
+            bool existe = true;
+
+            for (int i = 0; i < _contador; i++)
+            {
+                if (_Inventario[i].codigo == posicion)
+                {
+                    while (i < _contador)
+                    {
+                        _Inventario[i] = _Inventario[i + 1];
+                        i++;
+                    }
+                    _Inventario[_contador] = null;
+                    _contador--;
+                }
+                else
+                {
+                    existe = false;
+                }
+            }
+            return existe;
+        }
+
+        public string buscarProducto(int posicion)
+        {
+            string mostrar = "";
+            for (int i = 0; i < _contador; i++)
+            {
+                if (posicion - 1 == i)
+                {
+                    mostrar = _Inventario[i].ToString();
+                }
+            }
+            return mostrar;
+        }
+
+        public void insertarProducto(Producto producto, int posicion)
+        {
+            Producto producto2;
+            posicion = posicion - 1;
+            _contador++;
+
+            for (int i = posicion; i <_contador; i++)
+            {
+                //_Inventario[i] = _Inventario[i - 1];
+                auxiliar = _Inventario[i];
+                _Inventario[i] = producto;
+                producto = auxiliar;
+            }
+            //_contador++;
+            //_Inventario[posicion] = producto;
+        }
+
+        public override string ToString()
+        {
+            string mostrar = "";
+            for (int i = 0; i < _contador; i++)
+            {
+                mostrar += _Inventario[i].ToString();
+            }
+            return mostrar;
+        }
+    }
+}
